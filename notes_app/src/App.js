a154b93a75a8badf6e81975925e2a3f09d707440
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-02-24 17:52:06 +0500

    feat: React Router DOM implemented
    - Implemented React Router DOM for navigation.
    - Added routes for Menu and EmptyPlaceholder components.
    - Configured basic layout with a Router.

--- notes_app/src/App.js
+++ notes_app/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Menu from "./components/menu";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EmptyPlaceholder from "./components/empty-page";
+import Menu from "./components/menu";
 
 function App() {
   return (
-   <EmptyPlaceholder/>
+      <Router>
+        <div className="min-h-screen bg-gray-900 text-white flex flex-col">
+          <Routes>
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/empty-placeholder" element={<EmptyPlaceholder />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
